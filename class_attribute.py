29b899f28576e27cef190cc7925fe53bf8225676
newazsharif-dsi <newaz.sharif@dsinnovators.com>
2019-12-10 13:26:34 +0600

    Class attribute tutorial

---
+++ class_attribute.py
@@ -0,0 +1,37 @@
+class Employee:
+
+    increment = 1.04 # class variable
+
+    def __init__(self, first_name, last_name, email, payment):
+        # initialize instance variables
+        self.first_name = first_name;
+        self.last_name = last_name;
+        self.email = email;
+        self.payment = payment;
+
+    def full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def apply_raise(self):
+        self.payment = self.payment * self.increment #Access class attribute by instance
+
+
+
+#creating instances of Employee class
+employee_one = Employee("Newaz", "Sharif", "newaz.sharif&xyz.com", 10000)
+employee_two = Employee("Mohammad", "Ashraful", "md.ash&xyz.com", 20000)
+print(employee_one.full_name())
+
+
+print(employee_one.__dict__) #will print the namespace of employee_one instance
+print(Employee.__dict__) #will print the namespace of Employee class and we will see the 'increment' attribute
+
+employee_one.apply_raise()
+print(employee_one.payment)
+
+#Locally modifying 'increment' attribute
+employee_one.increment = 1.05
+
+print(Employee.increment)
+print(employee_one.increment)
+print(employee_two.increment)
